6fdc7f180c6d87e8a0d6e5fa6ba35b6ee087ff33
Joao Paulo Leal <jpll13@hotmail.com>
2024-03-15 17:40:00 -0300

    Page and Button

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ import flet as ft
 def main(pagina):
     pagina.title = "TestZap"
     texto =ft.Text("TestZap")
-    pagina.add(texto)
-    
-    
-    botao_entrar_chat= ft.ElevatedButton("Entrar no chat")
 
+
+    def login_chat(evento):
+        pass
+    
+    botao_entrar_chat= ft.ElevatedButton("Entrar no chat", on_click=login_chat)
+    
+    pagina.add(texto)
     pagina.add(botao_entrar_chat)
 
 ft.app(target=main, view= ft.AppView.WEB_BROWSER) # Com o port = 8000 e sem o "AppView." ficou mais rapido no primeiro teste
