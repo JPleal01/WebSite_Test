ec6282f32e60212a256095dd376c422f51602400
Joao Paulo Leal <jpll13@hotmail.com>
2024-03-14 21:56:26 -0300

    Step-by-step

---
+++ main.py
@@ -0,0 +1,6 @@
+#Definir se vai usar flat ou flask
+#Criar pagina 
+#Criar icones da primeira pagina
+    #Botão que gera caixa pra login
+#Atualizar pagina de maneira que não recarregue-a inteira
+#Criar conexão com a rede, de modo que estejam simuntaneas
